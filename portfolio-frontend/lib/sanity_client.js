af7fb4839044a37704e3e8bfdccc8a5f8fa1b234
MaDnNeSsKoOpi <96216773+MaDnNeSsKoOpi@users.noreply.github.com>
2022-12-24 23:58:13 +0200

    SEACOND commit

--- portfolio-frontend/lib/sanity_client.js
+++ portfolio-frontend/lib/sanity_client.js
@@ -4,7 +4,7 @@ import imageUrlBuilder from '@sanity/image-url'
 export const client = sanityClient({
     projectId: 'wp8twqhs',
     dataset: 'production',
-    token: 'sk8Rxx1GLCPlu1X8yp78Wi4xcqeyvlVBz4gjeYth2RGwbjkfAPE9Ye8ZP1x92DsiCOt7GvHFdRhEA27W0L7vijCpaytuwbdXKEz9LTR9QLodUEZDqN3CBsMWG9xnWgph9kT6TUGSJfPWmUPxvARNNtN9w9DIP9bpyJyaslfIGlWXPXX55KoS',
+    token: process.env.SANITY_TOKEN,
     useCdn: false,
     apiVersion: "2022-02-03"
 })
